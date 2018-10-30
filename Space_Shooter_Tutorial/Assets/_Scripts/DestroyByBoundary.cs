f1b4c2de5a17f6c61a49f606005f0928f5cd4cae
Kasey Carni <carni.k@knights.ucf.edu>
2018-10-30 02:38:05 -0400

    Working project

---
+++ Space_Shooter_Tutorial/Assets/_Scripts/DestroyByBoundary.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DestroyByBoundary : MonoBehaviour
+
+{
+
+        void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
